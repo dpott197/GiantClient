ffbc74cc4b55306f6bdfd2d609de6f6f0e4cd2f4
Darren Pottinger <darren.pottinger@gmail.com>
2017-09-04 16:24:11 -0700

    Changes to GameDetailActivity.

--- app/src/main/java/com/dpott/giantclient/ui/GameDetailActivity.kt
+++ app/src/main/java/com/dpott/giantclient/ui/GameDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.dpott.giantclient.C
 import com.dpott.giantclient.R
 import com.dpott.giantclient.data.GiantBombClient
@@ -12,16 +13,17 @@ import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_game_detail.*
 
 /**
  * Created by dpott on 8/13/2017.
  */
 
-class GameActivity : AppCompatActivity() {
+class GameDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game_detail)
         GiantBombClient.getInstance().getGameResponse(C.GAME_ID_HALO_COMBAT_EVOLVED)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
@@ -31,11 +33,15 @@ class GameActivity : AppCompatActivity() {
                     }
 
                     override fun onNext(gameResponse: GameResponse) {
-                        Toast.makeText(this@GameActivity, gameResponse.results.toString(), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@GameDetailActivity, gameResponse.results.toString(), Toast.LENGTH_SHORT).show()
+                        gameTitle.setText(gameResponse.results.name)
+                        Glide.with(this@GameDetailActivity)
+                                .load(gameResponse?.results?.images?.get(0)?.screenUrl)
+                                .into(gameImage)
                     }
 
                     override fun onError(e: Throwable) {
-                        Log.e("GameActivity", "", e)
+                        Log.e("GameDetailActivity", "", e)
                     }
 
                     override fun onComplete() {
